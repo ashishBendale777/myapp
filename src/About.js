0b2ea062d8f796aaff6e530994718c9d2efc32b9
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2023-10-07 16:04:01 +0530

    api get

--- src/About.js
+++ src/About.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 
 const About = () => {
+
+  const [usersData, setusersData] = useState([])
+
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/allusers')
+      .then((userData) => {
+        console.table(userData.data)
+        setusersData(userData.data)
+      }).catch((err) => {
+        console.log(err)
+      });
+  }, [])
+
+
   return (
-    <div className='page'>
-        <h1>About</h1>
+    <div>
+      <h1>USERS</h1>
+      <Container>
+        <Row>
+          {
+            usersData.map((user) => {
+              return (
+                <Col lg="4" md="6" sm="12">
+                  <Card>
+                    <Card.Title>{user.UserName}</Card.Title>
+                    <Card.Body>
+                      <p>{user.UserMobile}</p>
+                      <p>{user.UserGender}</p>
+                      <p>{user.UserEmail}</p>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
+      </Container>
     </div>
   )
 }
