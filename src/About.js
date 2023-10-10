5b8c06ca102b0fac577d15d8d145be69f4137583
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2023-10-10 16:32:57 +0530

    update

--- src/About.js
+++ src/About.js
@@ -1,11 +1,18 @@
+import { setSelectionRange } from '@testing-library/user-event/dist/utils'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Button, Card, Col, Container, Row } from 'react-bootstrap'
+import { Button, Card, Col, Container, Form, Modal, Row } from 'react-bootstrap'
 
 const About = () => {
 
   const [usersData, setusersData] = useState([])
-
+  const [isShow, setIsShow] = useState(false)
+  const [selectedUser, setselectedUser] = useState({
+    UserName: "",
+    UserGender: "",
+    UserEmail: "",
+    UserMobile: ""
+  })
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/allusers')
@@ -29,6 +36,23 @@ const About = () => {
       });
   }
 
+  const updateUser = () => {
+    const updata = {
+      uid: selectedUser._id,
+      UserMobile: selectedUser.UserMobile
+    }
+
+    axios.post("http://localhost:5000/api/updateuser", updata)
+      .then((result) => {
+        alert("User Updated")
+        setIsShow(false)
+        window.location.reload(false)
+
+      }).catch((err) => {
+        alert("Update fail")
+      });
+  }
+
   return (
     <div>
       <h1>USERS</h1>
@@ -48,6 +72,10 @@ const About = () => {
                     </Card.Body>
                     <Card.Footer>
                       <Button onClick={() => deleteUser(user._id)}>Delete</Button>
+                      <Button className='m-2' onClick={() => {
+                        setselectedUser(user)
+                        setIsShow(true)
+                      }}>Update</Button>
                     </Card.Footer>
                   </Card>
                 </Col>
@@ -56,6 +84,24 @@ const About = () => {
           }
         </Row>
       </Container>
+      <Modal show={isShow} onHide={() => setIsShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Update User
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Control type="text" value={selectedUser.UserName} placeholder='Enter Name' />
+            <Form.Control type="text" value={selectedUser.UserMobile}
+              onChange={(e) => setselectedUser({ ...selectedUser, UserMobile: e.target.value })}
+              placeholder='Enter Mobile' />
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => updateUser()}>Update</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
